3b19fbce2eac8abab070666054df3116d0135703
sirily11 <sirily1997@gmail.com>
2022-10-24 22:31:07 +0800

    first commit

---
+++ docs/intro.md
@@ -0,0 +1,20 @@
+---
+slug: /
+title: Introduction
+---
+
+# Welcome
+
+This is the tutorial page for your Web3 developer journey. We will be building a simple web3 app that allows you to mint NFTs. We will be covering the following topics:
+
+- Setting up your environment
+- Connecting to the Etherdata Blockchain network
+- Writing smart contracts
+- Compiling smart contracts
+- Deploying smart contracts
+- Interacting with smart contracts using react
+- Interacting with smart contracts using SwiftUI
+- Interacting with smart contracts using Flutter
+- Interacting with smart contracts using Kotlin
+
+We will divide each part into multiple smaller tutorials and provide a lot of interactive examples using the Codesandbox, a web-based IDE for web development, so you can follow along and play with the code. You should follow the order of the tutorials on the sidebar.
---
+++ docusaurus.config.js
@@ -0,0 +1,103 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "MSBD5017 tutorial",
+  tagline: "Learn, build, and Deploy",
+  url: "https://docs.msbd5017.etdchain.net",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "HKUST", // Usually your GitHub org/user name.
+  projectName: "MSBD5017", // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+  themes: ["@docusaurus/theme-live-codeblock"],
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "MSBD5017",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Tutorial",
+          },
+          {
+            href: "https://github.com/facebook/docusaurus",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} MSBD5017, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
