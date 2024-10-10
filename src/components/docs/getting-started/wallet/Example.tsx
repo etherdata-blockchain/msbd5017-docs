4d79f9ce9ce277eaecf9bf13310f0c9f2d42178c
sirily11 <32106111+sirily11@users.noreply.github.com>
2024-10-11 02:01:09 +0800

    fix: improve performance

--- src/components/docs/getting-started/wallet/Example.tsx
+++ src/components/docs/getting-started/wallet/Example.tsx
@@ -1,7 +1,20 @@
 import { session } from '@/actions/actions'
-import ConnectWalletExample from './1.ConnectWalletExample'
-import ConnectToTargetNetwork from './2.ConnectToTargetNetwork'
-import ClaimTestingToken from './3.ClaimTestingToken'
+import dynamic from 'next/dynamic'
+
+const ConnectWalletExample = dynamic(() => import('./1.ConnectWalletExample'), {
+  ssr: false,
+})
+
+const ConnectToTargetNetwork = dynamic(
+  () => import('./2.ConnectToTargetNetwork'),
+  {
+    ssr: false,
+  },
+)
+
+const ClaimTestingToken = dynamic(() => import('./3.ClaimTestingToken'), {
+  ssr: false,
+})
 
 export default async function Example() {
   const currentSession = await session()
